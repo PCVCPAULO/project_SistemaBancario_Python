92b12b2af6d8578152b1fab0a90393d533585ba3
PAULO C. V. COSTA <pcvcpaulo@gmail.com>
2025-08-11 22:35:37 -0300

    Criação da Distribuição.
    Criação da distribuição após a instalação da dependência "wheel", pois nas tentativas de criação estavam retornando erro.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    page_description = f.read() 
+
+setup(
+    name="sistema_bancario",
+    version="0.0.1",
+    author="PCVCPAULO",
+    author_email="pcvcpaulo@gmail.com",
+    description="Simula um sistema bancário",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PCVCPAULO/project_SistemaBancario_Python",
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
